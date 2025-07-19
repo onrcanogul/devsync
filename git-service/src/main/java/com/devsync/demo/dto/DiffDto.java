9be32bd86946c07111cc8faf3775ed46fc33f321
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-07-19 13:43:50 +0300

    first commit

---
+++ git-service/src/main/java/com/devsync/demo/dto/DiffDto.java
@@ -0,0 +1,19 @@
+package com.devsync.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class DiffDto {
+    private List<ChangedFileDto> changedFiles;
+
+    public DiffDto(List<ChangedFileDto> changedFiles) {
+        this.changedFiles = changedFiles;
+    }
+
+    public List<ChangedFileDto> getChangedFiles() { return changedFiles; }
+    public void setChangedFiles(List<ChangedFileDto> changedFiles) { this.changedFiles = changedFiles; }
+}
---
+++ git-service/src/main/java/com/devsync/demo/dto/PullRequestDto.java
@@ -0,0 +1,50 @@
+package com.devsync.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PullRequestDto {
+    private long id;
+    private String title;
+    private String author;
+    private String branch;
+    private List<CommitDto> commits;
+    private DiffDto diff;
+
+    public PullRequestDto(long id, String title, String author, String branch, List<CommitDto> commits, DiffDto diff) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.branch = branch;
+        this.commits = commits;
+        this.diff = diff;
+    }
+
+    public static class Base {
+        private Repo repo;
+    }
+
+    public static class Repo {
+        private String name;
+    }
+
+    private Base base;
+
+    public long getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getBranch() { return branch; }
+    public List<CommitDto> getCommits() { return commits; }
+    public DiffDto getDiff() { return diff; }
+
+    public void setId(long id) { this.id = id; }
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+    public void setBranch(String branch) { this.branch = branch; }
+    public void setCommits(List<CommitDto> commits) { this.commits = commits; }
+    public void setDiff(DiffDto diff) { this.diff = diff; }
+}
