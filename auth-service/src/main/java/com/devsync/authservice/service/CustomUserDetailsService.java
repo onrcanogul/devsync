2f8f426982868548d245ad163455de469ad34c67
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-07-22 22:01:31 +0300

    lombok bugs have been fixed

--- auth-service/src/main/java/com/devsync/authservice/service/CustomUserDetailsService.java
+++ auth-service/src/main/java/com/devsync/authservice/service/CustomUserDetailsService.java
@@ -1,24 +1,44 @@
 package com.devsync.authservice.service;
 
 import com.devsync.authservice.model.entity.User;
-import com.devsync.authservice.model.UserPrincipal;
 import com.devsync.authservice.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
+import jakarta.ws.rs.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepo;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepo.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-        return new UserPrincipal(user);
+    public UserDetails loadUserByUsername(String username) throws NotFoundException {
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username: " + username));
+
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
     }
 }
