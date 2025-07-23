91a0eb0a427fc2476bb6ab7e4b0a925efcae6189
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-07-23 22:38:58 +0300

    logging and performance aspects have been implemented

---
+++ analyze-service/src/main/java/com/devsync/analyzeservice/aop/PerformanceAspect.java
@@ -0,0 +1,21 @@
+package com.devsync.analyzeservice.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class PerformanceAspect {
+    @Around("execution(* com.devsync.analyzeservice.service.*.*(..))")
+    public Object measureExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long duration = System.currentTimeMillis() - start;
+        log.info("[PERFORMANCE] Method: {} executed in {} ms", joinPoint.getSignature().getName(), duration);
+        return result;
+    }
+}
