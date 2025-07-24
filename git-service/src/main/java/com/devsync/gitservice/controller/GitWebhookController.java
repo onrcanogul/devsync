faaf0bee670ba004fdcf77cd7016dad4dbb77315
onrcanogul <onurcanogul@ogr.eskisehir.edu.tr>
2025-07-24 21:09:32 +0300

    test pull request webhook

--- git-service/src/main/java/com/devsync/gitservice/controller/GitWebhookController.java
+++ git-service/src/main/java/com/devsync/gitservice/controller/GitWebhookController.java
@@ -1,7 +1,9 @@
 package com.devsync.gitservice.controller;
 
+import com.devsync.gitservice.dto.model.GithubWebhookModel;
 import com.devsync.gitservice.service.impl.GitServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,23 +13,18 @@ import java.util.Map;
 @RequestMapping("/api/git")
 public class GitWebhookController {
 
+    private final ObjectMapper objectMapper;
     private final GitServiceImpl gitService;
 
-    public GitWebhookController(GitServiceImpl gitService) {
+    public GitWebhookController(ObjectMapper objectMapper, GitServiceImpl gitService) {
+        this.objectMapper = objectMapper;
         this.gitService = gitService;
     }
 
     @PostMapping("/webhook/pull-request")
     public ResponseEntity<Void> handleWebhook(@RequestBody Map<String, Object> payload) throws JsonProcessingException {
-        String prId = String.valueOf(payload.get("number"));
-
-        Map<String, Object> repository = (Map<String, Object>) payload.get("repository");
-        String repo = (String) repository.get("name");
-
-        Map<String, Object> ownerMap = (Map<String, Object>) repository.get("owner");
-        String owner = (String) ownerMap.get("login");
-
-        gitService.handlePullRequest(owner, repo, prId);
+        GithubWebhookModel model = objectMapper.convertValue(payload, GithubWebhookModel.class);
+        gitService.handlePullRequest(model);
         return ResponseEntity.ok().build();
     }
 }
---
+++ git-service/src/main/java/com/devsync/gitservice/dto/model/Pusher.java
@@ -0,0 +1,9 @@
+package com.devsync.gitservice.dto.model;
+
+import lombok.Data;
+
+@Data
+public class Pusher {
+    private String name;
+    private String email;
+}
